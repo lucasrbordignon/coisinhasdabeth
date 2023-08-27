e7c1f92fa5cba9794fd353251a098d3c34e5b51b
lucasrbordigon <lucas.r.bordignon@gmail.com>
2023-08-27 10:38:34 -0300

    Configurando casas decimais de valores

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,34 +1,60 @@
 import Item from "@/components/Items"
 
 export default function Home() {
+  const items = [
+    {
+      "id": 1,
+      "name": "Esfoliante Capilar Limpeza Profunda Advance Techniques 125g",
+      "price": 97.00,
+      "isPromotional": true,
+      "promotionalPrice": 50.00,
+      "image": ""
+    },
+    {
+      "id": 1,
+      "name": "Esfoliante Capilar Limpeza Profunda Advance Techniques 125g",
+      "price": 40,
+      "isPromotional": true,
+      "promotionalPrice": 30,
+      "image": ""
+    },
+    {
+      "id": 1,
+      "name": "Esfoliante Capilar Limpeza Profunda Advance Techniques 125g",
+      "price": 62,
+      "isPromotional": true,
+      "promotionalPrice": 59,
+      "image": ""
+    },
+    {
+      "id": 1,
+      "name": "Esfoliante Capilar Limpeza Profunda Advance Techniques 125g",
+      "price": 39.99,
+      "isPromotional": true,
+      "promotionalPrice": 29.99,
+      "image": ""
+    }
+]
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left md:grid-cols-3 sm:grid-cols-2">
 
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
-
-        <Item />
+        {
+          items.map((item) => {
+            return (
+              <Item 
+                name={item.name} 
+                price={item.price}
+                isPromotional={item.isPromotional} 
+                promotionalPrice={item.promotionalPrice}
+              />
+            )
+          })
+        }
+      
       </div>
     </main>
   )
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,20 +1,46 @@
+import Decimal from 'decimal.js'
 import Image from "next/image"
 import Avon from '../assets/avon.jpg'
 
-export default function Item() {
+export default function Item(props) {
+
+  var price = new Decimal(props.price)
+  var promotionalPrice = new Decimal(props.promotionalPrice)
+  var difference = new Decimal(price.sub(promotionalPrice))
+
+  var discountPercentage = new Decimal(( difference * 100 ) / price)
+
+  const formattedResult = discountPercentage.toNumber().toLocaleString('pt-BR', {
+    style: 'decimal',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+   })
+
+  const formattedPrice = price.toNumber().toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
+  const formattedPromotionalPrice = promotionalPrice.toNumber().toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+
+  var name = props.name
+
   return (
     <a
       className="relative flex flex-col gap-2 rounded-lg border border-transparent px-5 py-4 transition-colors hover:cursor-pointer hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
     >
-
       <div>
 
-        <span 
-          className="absolute text-sm pr-1 top-2 right-4 bg-[#E966A0] rounded-r-sm rounded-l-2xl border-l-[10px] border-[#E966A0]"
-        >
-          33% OFF
-        </span>
-
+        {
+          <span 
+            className="absolute text-sm pr-1 top-2 right-4 bg-[#E966A0] rounded-r-sm rounded-l-2xl border-l-[10px] border-[#E966A0]"
+          >
+            {`${formattedResult}% OFF`}
+          </span>
+        }
         <Image
           alt="Product Image" 
           src={Avon} 
@@ -23,7 +49,7 @@ export default function Item() {
       </div>
       <h2 
         className={`mb-3 text-xs text-center opacity-90`}>
-        Esfoliante Capilar Limpeza Profunda Advance Techniques 125g        
+        {`${name.substr(0,50)}...`}
       </h2>
 
       <div>
@@ -31,12 +57,14 @@ export default function Item() {
           className={`m-0 max-w-[30ch] text-xs text-center line-through`}
           style={{color: 'rgb(233, 102, 160)'}}  
         >
-          R$ 39,99
+          {/* {`R$ ${props.price.toFixed(2)}`} */}
+          {formattedPrice}
         </p>
 
         <p 
           className={`m-0 max-w-[30ch] text-md text-center`}>
-          R$ 29,99
+          {/* {`R$ ${props.promotionalPrice.toFixed(2).toLocaleString()}`} */}
+          {formattedPromotionalPrice}
         </p>
       </div>
     </a>
